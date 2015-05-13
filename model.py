9ca1aeaddb91548c3e421601f3c9d578ccb4274c
Natalie <taliegarcia@gmail.com>
2015-05-12 18:21:29 -0700

    db populates with Meetup and GlobalGiving info. Wrote drafts of classes for other tables in model.py. Note: Check Flask-SQLA documentation RE: Many-to-Many/Assoc. Tables

--- model.py
+++ model.py
@@ -6,12 +6,85 @@ db = SQLAlchemy()
 
 
 ##############################################################################
-# Model definitions
+    ### Front End Models ###
+
+class User(db.Model):
+    """User of Apptivist website"""
+
+    __tablename__ = "users" 
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=True)
+    password = db.Column(db.String(30), nullable=True)
+    zipcode = db.Column(db.String(20), nullable=False) # essential for lookup, joining
+
+class Story(db.Model):
+    """Information on the news story posted by a user."""
+
+    __tablename__ = "stories" 
+
+    story_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    img_src = db.Column(db.String(200), nullable=True)
+    date = db.Column(db.DateTime(), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    user = db.relationship("User", backref=db.backref("stories", order_by=story_id))
+
+class Tag(db.Model):
+    """Tags tabl. The tag options are not yet defined on the website. 
+    User will be able to select multiple tag objects per story they post to the website."""
+
+    __tablename__ = "tags" 
+
+    tag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    meetup_id = db.Column(db.Integer, db.ForeignKey('meetups.meetup_id'))
+    gg_code = db.Column(db.String(64), db.ForeignKey('giving.gg_code'))
+
+    meetup = db.relationship("Meetup", backref=db.backref("tags", order_by=tag_id))
+    giving = db.relationship("GlobalGiving", backref=db.backref("tags", order_by=tag_id))
+
+
+##############################################################################
+    ### Association Models ###
+#FIXME
+## Do NOT make Association Models. See Flask-SQLAlchemy for references on how to handle many-to-many relationships:
+## https://pythonhosted.org/Flask-SQLAlchemy/models.html?highlight=many%20many 
+
+class StoryTag(db.Model):
+    """Association between news stories and tags"""
+
+    __tablename__ = "storytags" 
+
+    storytag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    story_id = db.Column(db.Integer, db.ForeignKey('stories.story_id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'))
+   
+    story = db.relationship("Story", backref=db.backref("storytags", order_by=storytag_id))
+    tag = db.relationship("Tag", backref=db.backref("storytags", order_by=storytag_id))
+
+
+class SimilarTag(db.Model):
+    """Association between 2 similar tags"""
+
+    __tablename__ = "similartags" 
+
+    similar_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    primary_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'))
+    secondary_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'))
+   
+    story = db.relationship("Story", backref=db.backref("storytags", order_by=storytag_id))
+    tag = db.relationship("Tag", backref=db.backref("storytags", order_by=storytag_id))
+
+##############################################################################
+    ### Models relying on API's ###
 
 class Meetup(db.Model):
     """Meetup.com object"""
 
-    __tablename__ = "meetup" 
+    __tablename__ = "meetups" 
 
     meetup_id = db.Column(db.Integer, primary_key=True)
     meetup_name = db.Column(db.String(64), nullable=True)
@@ -20,6 +93,7 @@ class Meetup(db.Model):
         """Provide helpful representation when printed. """
 
         return "<Meetup meetup_id=%s meetup_name=%s>" % (self.meetup_id, self.meetup_name)
+
 
 class GlobalGiving(db.Model):
     """Global Giving object"""
@@ -34,18 +108,28 @@ class GlobalGiving(db.Model):
 
         return "<GlobalGiving gg_code=%s gg_name=%s>" % (self.gg_code, self.gg_name)
 
-# class Congress(db.Model):
-#     """Congress contact object"""
 
-#     __tablename__ = "congress" 
+class Congress(db.Model):
+    """Congress contact object"""
 
-#     giving_code = db.Column(db.String(64), primary_key=True)
-#     giving_name = db.Column(db.String(64), nullable=True)
-    
-#     def __repr__(self):
-#         """Provide helpful representation when printed. """
+    __tablename__ = "congress" 
 
-#         return "<Giving giving_code=%s giving_name=%s>" % (self.giving_code, self.giving_name)
+    congress_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(64), nullable=True)
+    lname = db.Column(db.String(64), nullable=True)
+    phone = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(64), nullable=True)
+    fb = db.Column(db.String(64), nullable=True)
+    website = db.Column(db.String(200), nullable=True)
+    zipcode = db.Column(db.String(20), nullable=False) # essential for lookup, joining
+
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed. """
+
+        return "<Congress congress_id=%s congress_email=%s>" % (self.congress_id, self.congress_email)
+
 ##############################################################################
 # Helper functions
 
