21f130f2436c0dcaa35a604b1d09d884241193cf
Natalie <taliegarcia@gmail.com>
2015-05-14 14:04:31 -0700

    started writing server.py app routes. users can sign up and login. Users can click a button to see their congress members contact info, based on sunlight congress API.

--- server.py
+++ server.py
@@ -5,8 +5,9 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import Meetup, connect_to_db, db
+from model import User, Meetup, GlobalGiving, connect_to_db, db
 
+from suncongress import show_congress_contacts
 
 app = Flask(__name__)
 
@@ -16,6 +17,114 @@ app.secret_key = "ABC"
 # Normally, if you use an undefined variable in Jinja2, it fails silently.
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
+
+
+
+
+@app.route('/')
+def index():
+    """Display the homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route('/registration', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("reg_form.html")
+
+
+@app.route('/registration', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    # Get form variables
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    zipcode = request.form["zipcode"]
+
+    new_user = User(name=name, email=email, password=password, zipcode=zipcode)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    # automatically log in the new user:
+    session["user_id"] = new_user.user_id
+
+    flash("Welcome to the movement, %s. You are now logged in and ready to get active!" % name)
+
+    print User.query.filter_by(name=name).first()
+
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Show login form."""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login."""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+
+    return redirect("/apptivist/%s" % user.user_id)
+
+
+@app.route('/logout')
+def logout():
+    """Log out."""
+
+    del session["user_id"]
+    flash("Logged Out.")
+    return redirect("/")
+
+
+
+@app.route("/apptivist/<int:id>")
+def get_user_by_id(id):
+    """Display user info page by user_id
+    """
+    user = User.query.get(id)
+
+    return render_template("profile.html", user=user)
+
+
+@app.route("/congress")
+def display_congress():
+
+    user_id = session.get("user_id")
+    
+    if not user_id:
+        return render_template('congress.html', con_dict={"Error:":"Sorry you are not logged in"})
+
+    user = User.query.filter_by(user_id=user_id).first()
+    con_dict = show_congress_contacts(user.zipcode)
+    
+    return render_template('congress.html', con_dict=con_dict)
+
+        
 
 
 if __name__ == "__main__":
