e35e4b5e2cc48ff12075c3b3f65dfc0397581552
Natalie <taliegarcia@gmail.com>
2015-05-18 15:36:39 -0700

    working meetup function that returns a list of events based on search term climate-change

--- apis/meetup.py
+++ apis/meetup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     my_dict = gen_meetup_dict()
     print(my_dict)
 
-
+###############################################################################
 #### Exploration of Meetup API for Upcoming Events ####
 ## using python -i meetup.py ##
 events_url = "https://api.meetup.com/2/open_events?zip=94110&time=,1w&key=%s" % meetup_api_key
@@ -36,3 +36,24 @@ zipcode = '10012'
 climate_topics_url = 'https://api.meetup.com/2/open_events?zip=%s&topic=%s&page=20&key=%s' % (zipcode, climate, meetup_api_key)
 climate_results = requests.get(climate_topics_url)
 climate_dict = json.loads(climate_results.text)
+###############################################################################
+
+
+def climate_events(zipcode):
+    """Based on a zipcode, this will return a list of event info.
+    Each event's info is organized into dictionaries inside the master list."""
+
+    search_term = 'climate-change'
+    url = 'https://api.meetup.com/2/open_events?zip=%s&topic=%s&page=20&key=%s' % (zipcode, climate, meetup_api_key)
+    request = requests.get(url)
+    climate_json = json.loads(request.text)
+    events_list = climate_json['results']
+
+    for event in events_list:
+        print event['group']['name'].lstrip('0123456789')
+        print event['event_url']
+
+    return events_list
+
+
+
